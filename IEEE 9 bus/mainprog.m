0bf37067c65669fc0ec4a01bd93a874c4a8ee045
Farhad Khodaei <39498931+farhadkhodaei@users.noreply.github.com>
2021-10-16 16:32:29 +0330

    Add files via upload

--- IEEE 9 bus/mainprog.m
+++ IEEE 9 bus/mainprog.m
@@ -1,10 +1,10 @@
 errornum1=0;ddiv=7;
 clear regeig
-PL5=2.65;QL5=-0.75;
-PL6=0.25;QL6=0.5;
-PL8=0.25;QL8=0.5;
+PL5=1.25;QL5=0.5;
+PL6=0.9;QL6=0.3;
+PL8=1;QL8=0.35;
 
-HH1=11;HH2=11;HH3=2.2;
+HH1=7;HH2=7;HH3=7;
 for Pn2=5:10
     for Pn3=5:10
         P2=Pn2/ddiv;
